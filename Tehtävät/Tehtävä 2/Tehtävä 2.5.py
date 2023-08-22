7a49484f6f270398c622d9d719c80b5383476466
Samuli Lamminmäki <samuli.lamminmaki@hotmail.com>
2023-08-22 13:35:10 +0300

    Tehtävä 2.5

--- Tehtävät/Tehtävä 2/Tehtävä 2.5.py
+++ Tehtävät/Tehtävä 2/Tehtävä 2.5.py
@@ -3,15 +3,16 @@ naulalkm = float(input("Anna naulat: "))
 luotilkm = float(input("Anna luodit: "))
 
 
-
-
 luoditg = float(13.3 * luotilkm)
 naulatg = float(32 * 13.3 * naulalkm)
 leiviskätg = float((32 * 13.3 * 20)*leiviskälkm)
 
 
-kg = float(luoditg+naulatg+leiviskätg)
-print(str(kg))
+massa = float(luoditg+naulatg+leiviskätg)
+massag = massa % 1000
+massakg = int (massa / 1000)
+
+print(f"Massa nykymittojen mukaan: \n {massakg} kilogrammaa ja  {massag:.2f} grammaa.")
 
 
 
