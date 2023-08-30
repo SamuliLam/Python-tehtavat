a53597cd900caba63b35a39a78358b2c0a8fea99
Samuli <samuli.lamminmaki@metropolia.fi>
2023-08-30 20:45:22 +0300

    Tehtävät 6.3 - 6.5

--- Tehtävät/6. Funktio/Tehtävä 6.3.py
+++ Tehtävät/6. Funktio/Tehtävä 6.3.py
@@ -2,9 +2,11 @@ import math
 
 gallons = input("Anna bensiinin määrä nestegallonoina: ")
 
+
 def gallon_to_liter_converter(gallonamount):
-    liters = float(gallonamount * 3.785)
-    return liters
+    return print(gallonamount * 3.785)
+
 
 while gallons != "":
-    gallon_to_liter_converter(gallons)
+    gallon_to_liter_converter(float(gallons))
+    gallons = input("Anna bensiinin määrä nestegallonoina: ")
---
+++ Tehtävät/6. Funktio/Tehtävä 6.5.py
@@ -0,0 +1,18 @@
+userInput = input("Anna numero: ")
+
+numbers = []
+
+while userInput != "":
+    numbers.append(float(userInput))
+    userInput = input("Anna numero: ")
+
+
+def list_even(array):
+    sum = 0
+    for evennumber in range(array, 2):
+
+
+        return print(evennumber)
+
+
+list_even(numbers)
