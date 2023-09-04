989ffe799ec380d5682715c983c11001f7b74473
Samuli <samuli.lamminmaki@metropolia.fi>
2023-09-04 12:18:39 +0300

    7.3

---
+++ Tehtävät/7. Monikko, joukko ja sanakirja/Tehtävä 7.3.py
@@ -0,0 +1,22 @@
+
+lentoasemat = {}
+
+while True:
+    choice = input(
+        "1. Syötä uusi lentoasema \n"
+        "2. Hae lentoaseman tiedot \n"
+        "3. Lopeta ohjelma \n"
+        "Kirjoita haluamaasi syötettä vastaava numero: \n"
+    )
+
+    if choice == "1":
+        lentoasema_nimi = input("Anna lentoaseman nimi: ")
+        icao_koodi = input("Anna lentoaseman ICAO-koodi: ")
+        lentoasemat[icao_koodi] = lentoasema_nimi
+
+    elif choice == "2":
+        search_icao = input("Anna sen lentoaseman ICAO-koodi, jonka tiedot haluat: \n")
+        if search_icao in lentoasemat:
+            print("Lentoaseman nimi: " + lentoasemat[search_icao])
+    elif choice == "3":
+        break
